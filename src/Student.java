d44532653a0881e5c5501b82c903d24ca7dbfa5a
MatthewHinton56 <Matthewhinton56@gmail.com>
2018-02-15 00:49:56 -0600

    Modification of Visualization
    modified to show the path of students

--- src/Student.java
+++ src/Student.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Comparator;
 
 public class Student {
 
@@ -9,14 +10,14 @@ public class Student {
 	private String id;
 	private String gender;
 	private boolean URM;
-	private String intialMajor;
+	private String initialMajor;
 	private ArrayList<String> majorAfterEachYear;
 	
 	public Student(String id, String gender, boolean uRM, String intialMajor) {
 		this.id = id;
 		this.gender = gender;
 		URM = uRM;
-		this.intialMajor = intialMajor;
+		this.initialMajor = intialMajor;
 		majorAfterEachYear = new ArrayList<String>();
 	}
 	
@@ -25,6 +26,8 @@ public class Student {
 	}
 	
 	public String getYear(int year) {
+		if(year==-1)
+			return initialMajor;
 		return majorAfterEachYear.get(year);
 	}
 
@@ -41,7 +44,72 @@ public class Student {
 	}
 
 	public String getIntialMajor() {
-		return intialMajor;
+		return initialMajor;
 	}
+
+	@Override
+	public boolean equals(Object obj) {
+		// TODO Auto-generated method stub
+		return this.id.equals(((Student)obj).id);
+	}
+
 	
+
+	
+}
+
+class StudentComparator implements Comparator<Student> 
+{
+
+	Department department;
+	int year;
+	public StudentComparator(int year, Department department) {
+		this.department = department;
+		this.year = year;
+	}
+	@Override
+	public int compare(Student o1, Student o2) {
+		String major1, major2;
+		if(year != 6 && o1.getYear(year).equals(o2.getYear(year))) {
+			major1 = o1.getYear(year+1);
+			major2 = o2.getYear(year+1);
+		} else {
+			major1 = o1.getYear(year);
+			major2 = o2.getYear(year);
+		}
+		
+		boolean major1IsDismissed = Department.isDismissed(major1);
+		boolean major2IsDismissed = Department.isDismissed(major2);
+		if(major1IsDismissed)
+			return 1;
+		if(major2IsDismissed)
+			return -1;
+
+		boolean major1IsDropOut = Department.isDropOut(major1);
+		boolean major2IsDropOut = Department.isDropOut(major2);
+
+		if(major1IsDropOut)
+			return 1;
+		if(major2IsDropOut)
+			return -1;
+
+		boolean major1IsCNS = Department.isCNS(major1);
+		boolean major2IsCNS = Department.isCNS(major2);
+		if(!major1IsCNS)
+			return 1;
+		if(!major2IsCNS)
+			return -1;
+		//System.out.println(department);
+		boolean major1IsDepartment = department.hasMajor(major1);
+		boolean major2IsDepartment = department.hasMajor(major2);
+		if(!major1IsDepartment)
+			return 1;
+		if(!major2IsDepartment)
+			return -1;
+		return major2.compareTo(major1);
+	}
+
+	
+	
+
 }
--- src/Visualization.java
+++ src/Visualization.java
@@ -2,6 +2,9 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.util.ArrayList;
@@ -19,12 +22,14 @@ public class Visualization extends JPanel{
 	private static final int TOWERWIDTH = 75;
 	private Map<String, Color> majorColors;
 	private Cohort cohort;
+	private String[][] majorMap;
+	private ArrayList<ArrayList<Student>> studentTowers;
 	
 	private BufferedImage canvas;
 	int height;
 	int width;
 	public Visualization(int width, int height) {
-
+		this.addMouseMotionListener(new VisualMouseListener());
 		JFileChooser chooser = new JFileChooser();
 		File selectedFile = new File("CSCohortTest.xlsx");
 		/*if (chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION)
@@ -32,15 +37,65 @@ public class Visualization extends JPanel{
 		selectedFile = chooser.getSelectedFile();
 		}*/
 		cohort = new Cohort(selectedFile);
+		generateStudentTowers(cohort.getTotalStudents());
 		constructColorMap();
         canvas = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
         this.height=height;
         this.width=width;
+        //majorMap = new String[height][width];
         fillCanvas(Color.WHITE);
         towerConstruction(cohort.getTotalMajorCountsInitial(), cohort.getTotalMajorCountsPerYear(), cohort.getTotalStudents().size());
+        lineConstruction();
     }
 
-    public Dimension getPreferredSize() {
+    private void generateStudentTowers(ArrayList<Student> studentI) {
+    	studentTowers = new ArrayList<ArrayList<Student>>();
+		for(int i = -1; i < cohort.getTotalMajorCountsPerYear().size();i++) {
+			ArrayList<Student> students = new ArrayList<Student>();
+			students.addAll(studentI);
+			students.sort(new StudentComparator(i, cohort.getDepartment()));
+			
+			studentTowers.add(students);
+		}
+		
+	}
+    
+    private void lineConstruction() {
+    	int total = studentTowers.get(0).size();
+    	float percent = ((float)TOWERHEIGHT)/total;
+    	int towerTop = 200;
+		//System.out.println(count);
+		int dy = Math.round(percent);
+		//System.out.println(dy);
+		for(int pos = 1;pos < studentTowers.size(); pos++ ) {
+			int x = 59 + 200 * pos;
+			int x0 = x - 134;
+			for(int sPos = 0; sPos < total; sPos++) {
+				if(pos == 4)
+					System.out.println(sPos);
+				Student s = studentTowers.get(pos).get(sPos);
+				String major = s.getYear(pos-1);
+				Color c = majorColors.get(major);
+				int y = sPos*dy+205;
+				int y0 = studentTowers.get(pos - 1).indexOf(s)*dy+200;
+				/*if(y0 > y)
+				{
+					int temp = y;
+					 y = y0;
+					 y0 = temp;
+				}*/
+				if(pos == 4)
+					System.out.println(y0 + " "+ y);
+				for(int yShift = 0; yShift < 5; yShift++)
+					putLineDDA(x0,y0+yShift,x,y+yShift,bleach(c,(float) .5),major);
+			}
+		}
+    }
+    
+    
+    
+
+	public Dimension getPreferredSize() {
         return new Dimension(canvas.getWidth(), canvas.getHeight());
     }
 
@@ -54,17 +109,19 @@ public class Visualization extends JPanel{
     	canvas.setRGB(x, y, Color.BLACK.getRGB());
     }
     
-    public void drawRectangle(int x0, int y0, int x, int y, Color c) {
+    public void drawRectangle(int x0, int y0, int x, int y, Color c, String s) {
     	for(int dx = x0; dx < x;dx++) 
     	{	
     		for(int dy = y0; dy > y; dy--)
     		{
     			canvas.setRGB(dx, dy, c.getRGB());
+    			majorMap[dy][dx] = s;
     		}
     	}	
     }
 	
-	public void putLineDDA(int x0, int y0, int x, int y, Color c) {
+	public void putLineDDA(int x0, int y0, int x, int y, Color c, String major) {
+		//System.out.println(x0 + " "+y0);
 		canvas.setRGB(x0, y0, c.getRGB());
 		canvas.setRGB(x, y, c.getRGB());
 		int dx = x-x0;
@@ -73,33 +130,39 @@ public class Visualization extends JPanel{
 		float xT = x0;
 		float yT = y0;
 		if(Math.abs(dx) > Math.abs(dy)) 
-			steps = dx;
+			steps = Math.abs(dx);
 		else
-			steps = dy;
+			steps = Math.abs(dy);
 		float xIncrement = ((float)dx)/steps;
 		float yIncrement = ((float)dy)/steps;
 		for(int v=0; v < steps; v++)
 		{
 		   xT = xT + xIncrement;
 		   yT = yT + yIncrement;
-		   canvas.setRGB(Math.round(xT), Math.round(yT),c.getRGB());
+		   if(majorMap[ Math.round(yT)][ Math.round(xT)] == null) {
+			   majorMap[ Math.round(yT)][ Math.round(xT)] = major; 
+			   canvas.setRGB(Math.round(xT), Math.round(yT),c.getRGB());
+		   }
+			   
 		}
 		repaint();
 	}
 	
 	public void constructSankeyTower(int x0, int y0,int total, Map<String, Integer> counts) {
 		for(String s: counts.keySet()) {
+			System.out.println(s);
 			Color c = majorColors.get(s);
 			int count = counts.get(s);
 			float percent = ((float)count)/total;
 			//System.out.println(count);
 			int dy = Math.round(percent*TOWERHEIGHT);
-			drawRectangle(x0,y0,x0+TOWERWIDTH,y0-dy,c);
+			drawRectangle(x0,y0,x0+TOWERWIDTH,y0-dy,c, s);
 			y0-=dy;
 		}
 	}
 	
 	public void towerConstruction(Map<String, Integer> initialCounts, ArrayList<Map<String, Integer>> counts, int total) {
+		majorMap = new String[canvas.getHeight()][canvas.getWidth()];
 		constructSankeyTower(50, 700, total,initialCounts);
 		for(int pos = 0; pos < counts.size(); pos++) {
 			constructSankeyTower(250+200*pos, 700,total, counts.get(pos));
@@ -149,6 +212,49 @@ public class Visualization extends JPanel{
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 	
-	
+	private class VisualMouseListener implements MouseMotionListener {
+
+		private String currentMajor;
+		//private int year;
+		
+		
+		@Override
+		public void mouseDragged(MouseEvent e) {}
+
+		@Override
+		public void mouseMoved(MouseEvent e) {
+			int x = e.getX();
+			int y = e.getY();
+			//System.out.println(x+" "+y);
+			if( x >= 0 && y >= 0 && x < majorMap[0].length && y < majorMap.length) {
+			String major = majorMap[y][x];
+			//System.out.println(major);
+			if(currentMajor != null && !currentMajor.equals(major)) {
+				for(int dx = 0; dx < majorMap[0].length;dx++)
+					for(int dy = 0; dy < majorMap.length;dy++ ) {
+						if(currentMajor.equals(majorMap[dy][dx])) {
+							canvas.setRGB(dx, dy, majorColors.get(currentMajor).getRGB());
+						}
+						if(major != null && major.equals(majorMap[dy][dx])) {
+							canvas.setRGB(dx, dy, (new Color(255,255,0)).getRGB());
+						}
+					}
+			}
+			if(currentMajor == null && major != null) {
+				for(int dx = 0; dx < majorMap[0].length;dx++)
+					for(int dy = 0; dy < majorMap.length;dy++ ) {
+						if(major.equals(majorMap[dy][dx])) {
+							//System.out.println(dx+" "+dy);
+							canvas.setRGB(dx, dy, (new Color(255,255,0)).getRGB());
+						}
+						
+					}
+			}
+			currentMajor = major;
+			Visualization.this.repaint();
+			}
+ 		}
+		
+	}
 	
 }
